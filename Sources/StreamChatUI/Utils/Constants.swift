93555791ef8eba8f2dab0cb1506cb64c428d6478
Md Hanif <mohammed.h@simformsolutions.com>
2022-05-09 17:56:06 +0530

    - remove duplicates

--- Sources/StreamChatUI/Utils/Constants.swift
+++ Sources/StreamChatUI/Utils/Constants.swift
@@ -21,6 +21,7 @@ public struct Constants {
     public static var decimalSeparator: String {
         return Locale.current.decimalSeparator ?? "."
     }
+    public static let giphyBaseUrl = "https://api.giphy.com/"
 }
 
 public struct UserdefaultKey {
@@ -28,21 +29,4 @@ public struct UserdefaultKey {
     public static let visibleSticker = "visibleSticker"
     public static let recentSticker = "recentSticker"
 }
-
-// MARK: Constant key
-public let kExtraDataChannelDescription = "channelDescription"
-public let kExtraDataOneToOneChat = "OneToOneChat"
-public let kExtraDataIsGroupChat = "DataIsGroupChat"
-
-public let kInviteGroupID = "kInviteGroupID"
-public let kInviteId = "kInviteId"
-public let kInviteExpiryDate = "kInviteExpiryDate"
-
-public let kPrivateGroupPasscode = "kPrivateGroupPasscode"
-public let kPrivateGroupLat = "kPrivateGroupLat"
-public let kPrivateGroupLon = "kPrivateGroupLon"
-public let kGroupId = "kGroupId"
-
-// MARK: - Base url's
-public let giphyBaseUrl = "https://api.giphy.com/"
 
